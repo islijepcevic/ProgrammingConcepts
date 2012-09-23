301010c5431ce90061e3726b7510396114a8293a
islijepcevic <islijepcevic@gmail.com>
2012-09-24 01:00:15 +0200

    first commit; everything is set up to work in eclipse; hello world program is done

---
+++ chapter-01/exercise_1_1.cpp
@@ -0,0 +1,20 @@
+/*
+ * exercise_1_1.cpp
+ *
+ * Hello World
+ *
+ * Created on: Sept 23, 2012
+ * 	   Author: Ivan Slijepcevic
+ * 	   																					   (and e-mail) here
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	// print Hello World!
+	std::cout << "Hello World!\n";
+
+    return 0;
+}
+
